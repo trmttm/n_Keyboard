8103dd1774acdf5a38853021059ac03a013b7ff5
trmttm <96355189+trmttm@users.noreply.github.com>
2023-06-27 13:29:35 +0900

    Bug fix

--- n_keyboard/app/define_objects_interactions.py
+++ n_keyboard/app/define_objects_interactions.py
@@ -5,8 +5,8 @@ from n_keyboard.gui.state import State
 
 def define_objects_interactions(input_display: KeyboardInputDisplay, state: State):
     state.attach(c.STATE, lambda value: input_display.label_state_display.configure(text=value))
-    state.attach(c.CHAR, lambda value: input_display.label_state_display.configure(text=value))
-    state.attach(c.KEYSYM, lambda value: input_display.label_state_display.configure(text=value))
-    state.attach(c.KEYSYM_NUM, lambda value: input_display.label_state_display.configure(text=value))
-    state.attach(c.KEYCODE, lambda value: input_display.label_state_display.configure(text=value))
+    state.attach(c.CHAR, lambda value: input_display.label_char_display.configure(text=value))
+    state.attach(c.KEYSYM, lambda value: input_display.label_keysysm_display.configure(text=value))
+    state.attach(c.KEYSYM_NUM, lambda value: input_display.label_keysym_num_display.configure(text=value))
+    state.attach(c.KEYCODE, lambda value: input_display.label_keycode_display.configure(text=value))
     input_display.attach_to_switch(lambda on_off: state.toggle_user_input_capture(on_off))
