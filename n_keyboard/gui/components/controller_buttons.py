a21aa38b98dd8d4940f6e90adfd9e247bd980db2
trmttm <96355189+trmttm@users.noreply.github.com>
2023-06-27 17:00:13 +0900

    Add padding.

--- n_keyboard/gui/components/controller_buttons.py
+++ n_keyboard/gui/components/controller_buttons.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 
 class ControllerButtons:
     def __init__(self, parent_frame: ttk.Frame):
-        root = ttk.Frame(parent_frame)
+        root = ttk.Frame(parent_frame, padding=(0, 15))
         self.button_ok = ttk.Button(root, text='OK')
         self.button_ok.grid(row=0, column=0)
         root.grid(row=1, column=0, columnspan=2)
